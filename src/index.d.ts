4835434a83f567a3748b7f4d827cfe1a8980d562
Reiosantos <ronireiosantos@gmail.com>
2020-03-04 15:58:35 +0300

    Setup Package

---
+++ src/index.d.ts
@@ -0,0 +1,1 @@
+export function SanToCamelCase(): string;
---
+++ src/index.ts
@@ -0,0 +1,55 @@
+class ToCamelCase {
+  private replace = (value: string) => {
+    let ct = value.charAt(0).toLowerCase();
+    const val = value.substr(1);
+    ct =
+      ct +
+      val.replace(/([-_][a-zA-Z0-9])/g, group =>
+        group
+          .toUpperCase()
+          .replace('-', '')
+          .replace('_', ''),
+      );
+    return ct;
+  };
+
+  private transformObject = (value: any) => {
+    const keys = Object.keys(value) as string[];
+
+    keys.forEach((oldKey: string) => {
+      const newKey: string = this.replace(oldKey);
+
+      if (Array.isArray(value[oldKey])) {
+        value[newKey] = this.transformArray(value[oldKey]);
+      } else if (typeof value[oldKey] === 'object') {
+        value[newKey] = this.transformObject(value[oldKey]);
+      } else {
+        value[newKey] = value[oldKey];
+      }
+
+      if (newKey !== oldKey) delete value[oldKey];
+    });
+    return value;
+  };
+
+  private transformArray = (value: object[]) => {
+    return value.map(this.transformObject);
+  };
+
+  public transform(value: any, ...args: any[]): any {
+    if (!value) {
+      return value;
+    }
+    if (Array.isArray(value)) {
+      return this.transformArray(value);
+    }
+    if (typeof value === 'object') {
+      return this.transformObject(value);
+    }
+    return this.replace(value);
+  }
+}
+
+const { transform } = new ToCamelCase();
+
+export { transform as SanToCamelCase };
