3c3e066be4c526ebf3216f76bee4fae8185e8c63
Reiosantos <ronireiosantos@gmail.com>
2020-03-04 19:38:38 +0300

    Fix typescript configs Fix Closure causing undefined

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 class ToCamelCase {
-  replace = (value: string) => {
+  transform = (value: any, ...args: any[]): any => {
+    if (!value) {
+      return value;
+    }
+    if (Array.isArray(value)) {
+      return this.transformArray(value);
+    }
+    if (typeof value === 'object') {
+      return this.transformObject(value);
+    }
+    return this.replace(value);
+  };
+
+  private replace = (value: string) => {
     let ct = value.charAt(0).toLowerCase();
     const val = value.substr(1);
     ct =
@@ -13,7 +26,7 @@ class ToCamelCase {
     return ct;
   };
 
-  transformObject = (value: any) => {
+  private transformObject = (value: any) => {
     const keys = Object.keys(value) as string[];
 
     keys.forEach((oldKey: string) => {
@@ -32,22 +45,9 @@ class ToCamelCase {
     return value;
   };
 
-  transformArray = (value: object[]) => {
+  private transformArray = (value: object[]) => {
     return value.map(this.transformObject);
   };
-
-  transform(value: any, ...args: any[]): any {
-    if (!value) {
-      return value;
-    }
-    if (Array.isArray(value)) {
-      return this.transformArray(value);
-    }
-    if (typeof value === 'object') {
-      return this.transformObject(value);
-    }
-    return this.replace(value);
-  }
 }
 
 const { transform } = new ToCamelCase();
